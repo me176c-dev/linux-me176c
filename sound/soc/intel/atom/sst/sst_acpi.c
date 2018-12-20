0af5fe0d12fe2e1a980f4221d3e59250abb2907c
lambdadroid <lambdadroid@gmail.com>
2018-12-20 19:15:15 +0100

    ASoC: Intel: sst: Fallback to BYT-CR if IRQ 5 is missing

--- sound/soc/intel/atom/sst/sst_acpi.c
+++ sound/soc/intel/atom/sst/sst_acpi.c
@@ -255,9 +255,21 @@ static int is_byt(void)
 	return status;
 }
 
-static int is_byt_cr(struct device *dev, bool *bytcr)
+static int is_byt_cr(struct platform_device *pdev, bool *bytcr)
 {
+	struct device *dev = &pdev->dev;
 	int status = 0;
+
+	if (platform_get_resource(pdev, IORESOURCE_IRQ, 5) == NULL) {
+		/*
+		 * Some devices detected as BYT-T have only a single IRQ listed,
+		 * causing platform_get_irq with index 5 to return -ENXIO.
+		 * The correct IRQ in this case is at index 0, as used on BYT-CR.
+		 */
+		dev_info(dev, "Falling back to Baytrail-CR platform\n");
+		*bytcr = true;
+		return status;
+	}
 
 	if (IS_ENABLED(CONFIG_IOSF_MBI)) {
 		u32 bios_status;
@@ -278,9 +290,10 @@ static int is_byt_cr(struct device *dev, bool *bytcr)
 			/* bits 26:27 mirror PMIC options */
 			bios_status = (bios_status >> 26) & 3;
 
-			if ((bios_status == 1) || (bios_status == 3))
+			if ((bios_status == 1) || (bios_status == 3)) {
+				dev_info(dev, "Detected Baytrail-CR platform\n");
 				*bytcr = true;
-			else
+			} else
 				dev_info(dev, "BYT-CR not detected\n");
 		}
 	} else {
@@ -333,10 +346,8 @@ static int sst_acpi_probe(struct platform_device *pdev)
 	if (ret < 0)
 		return ret;
 
-	ret = is_byt_cr(dev, &bytcr);
+	ret = is_byt_cr(pdev, &bytcr);
 	if (!((ret < 0) || (bytcr == false))) {
-		dev_info(dev, "Detected Baytrail-CR platform\n");
-
 		/* override resource info */
 		byt_rvp_platform_data.res_info = &bytcr_res_info;
 	}
